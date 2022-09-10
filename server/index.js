f94a22253726198f55baffae7548777920f0f098
Qingzhou Yan <qingzhouyan@gmail.com>
2022-09-10 10:41:54 -0700

    clean the server index

--- server/index.js
+++ server/index.js
@@ -8,10 +8,15 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+});
+
 app.use('/products', express.static(path.join(__dirname, '../client/dist/')));
 
 app.get('*', (req, res) => {
-  res.status('404').send('<h1 style="text-align:center; margin: 200px">......404 Not Found</h1>');
+  res.status(404).send('<h1 style="text-align:center; margin: 200px">......404 Not Found</h1>');
 });
 
 const PORT = process.env.PORT || 3030;
